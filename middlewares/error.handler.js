2732f47de585250e437780cb992b5f4247dd691a
Nicolas Molina Monroy <hi@nicobytes.com>
2023-02-21 11:45:04 -0400

    :construction: save

--- middlewares/error.handler.js
+++ middlewares/error.handler.js
@@ -14,8 +14,9 @@ function boomErrorHandler(err, req, res, next) {
   if (err.isBoom) {
     const { output } = err;
     res.status(output.statusCode).json(output.payload);
+  } {
+    next(err);
   }
-  next(err);
 }
 
 
