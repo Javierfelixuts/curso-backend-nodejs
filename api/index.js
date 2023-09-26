663682643e0d8f831861862c60f0a44cce42d54b
Javier_Felix <felixtorresjavierantonio@gmail.com>
2023-09-25 18:10:39 -0700

    updated

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-const whitelist = ['http://localhost:8080', 'https://myapp.co'];
+const whitelist = ['http://localhost:8080'];
 const options = {
   origin: (origin, callback) => {
     if (whitelist.includes(origin) || !origin) {
@@ -38,4 +38,54 @@ app.use(errorHandler);
 
 app.listen(port, () => {
   console.log('Mi port' +  port);
+});
+
+
+
+
+
+
+
+
+
+const http = require('http');
+
+//const config = require("./app/config/config.js");
+const config = {};
+
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get("/", (req, res) => {
+    res.json({ success: true, message: "Server node js" });
+});
+
+
+app.post("/lessons", (req, res) => {
+    console.log('update');
+    if (!req.body) {
+        res.status(400).send({
+            message: "Content can not be empty!"
+        });
+    }
+    config.SOKECT_IO.emit('updateLesson', req.body);
+    res.json(req.body);
+});
+
+
+var server = http.createServer(app);
+var io = require('socket.io')(server, {
+    transports: ['websocket', 'polling'],
+    cors: {
+        origin: '*',
+    }
+});
+config.SOKECT_IO = io;
+
+server.listen(app.get('port'), () => {
+    console.log('Running server');
+});
+
+io.on('connect', async(socket) => {
+    console.log('Connected socket');
 });
